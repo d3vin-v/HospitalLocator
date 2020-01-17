661d706d0db6bbafea633dfb33438ae30c6b8619
d3vin-v <devinje01@gmail.com>
2020-01-17 12:28:15 -0600

    Day 2 Assignment

--- CityInAustralia.java
+++ CityInAustralia.java
@@ -1,5 +1,26 @@
 class CityInAustralia {
-	
-    
+	private String name;
+	private int pop;
+	private boolean coastal;
+	private String output = "";
 
+	public CityInAustralia(String n, int p, boolean c) {
+		name = n;
+		pop = p;
+		coastal = c;
+	}
+	
+	
+	
+	
+	public String toString() {
+		output += name + "\t";
+		output += pop + "\t";
+		if(coastal) {
+			output += "Coastal";
+		}else if(!coastal) {
+			output += "Inland";
+		}
+		return output;
+	}
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
-  public static void main(String[] args) {
-    
+	public static void main(String[] args) {
+
+		CityInAustralia c1 = new CityInAustralia("Sydney", 500, true);
 		
-  }
+		System.out.println(c1);
+	}
 }
