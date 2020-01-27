ccc127b6764d862f53bf5213693b5630eff8e689
d3vin-v <devinje01@gmail.com>
2020-01-27 12:42:48 -0600

    City constructors complete

--- Main.java
+++ Main.java
@@ -9,16 +9,16 @@ class Main {
 		ArrayList<Beaches> beaches = new ArrayList<Beaches>();
 		ArrayList<Hospitals> hosps = new ArrayList<Hospitals>();
 
-		CityInAustralia c1 = new CoastalCity("Darwin", 148564, .6, hosps);
-		CityInAustralia c2 = new CoastalCity("Sydney", 5230000, 10.5, hosps);
-		CityInAustralia c3 = new CoastalCity("Brisbane", 2274560, 20.6, hosps);
-		CityInAustralia c4 = new CoastalCity("Melbourne", 4936000, 5.5, hosps);
-		CityInAustralia c5 = new CoastalCity("Perth", 2059484, 12, hosps);
-		CityInAustralia c6 = new InlandCity("Rockhampton", 80665, 29.2, hosps);
-		CityInAustralia c7 = new InlandCity("Bendingo", 99122, 6.4, hosps);
-		CityInAustralia c8 = new InlandCity("Toowoomba", 134037, 39.4, hosps);
-		CityInAustralia c9 = new InlandCity("Wagga Wagga", 64820, 29.2, hosps);
-		CityInAustralia c10 = new InlandCity("Alice Springs", 29534, 5, hosps);
+		CityInAustralia c1 = new CoastalCity("Darwin", 148564, .6, hosps, beaches);
+		CityInAustralia c2 = new CoastalCity("Sydney", 5230000, 10.5, hosps, beaches);
+		CityInAustralia c3 = new CoastalCity("Brisbane", 2274560, 20.6, hosps, beaches);
+		CityInAustralia c4 = new CoastalCity("Melbourne", 4936000, 5.5, hosps, beaches);
+		CityInAustralia c5 = new CoastalCity("Perth", 2059484, 12, hosps, beaches);
+		CityInAustralia c6 = new InlandCity("Rockhampton", 80665, 29.2, hosps, parks);
+		CityInAustralia c7 = new InlandCity("Bendingo", 99122, 6.4, hosps, parks);
+		CityInAustralia c8 = new InlandCity("Toowoomba", 134037, 39.4, hosps, parks);
+		CityInAustralia c9 = new InlandCity("Wagga Wagga", 64820, 29.2, hosps, parks);
+		CityInAustralia c10 = new InlandCity("Alice Springs", 29534, 5, hosps, parks);
 		
 		ArrayList<CityInAustralia> cities = new ArrayList<CityInAustralia>();
 		cities.add(c1);
